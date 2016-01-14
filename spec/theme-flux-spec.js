ec955a5e57d5a389add86fcbd4069e09298c5913
Antonio Scandurra <me@as-cii.com>
2016-01-14 10:55:34 -0700

    Update theme with a timeout of 100ms
    This allows Atom to gracefully change themes 
    without going haywire.

--- spec/theme-flux-spec.js
+++ spec/theme-flux-spec.js
@@ -13,22 +13,27 @@ describe('ThemeFlux', () => {
     atom.config.set('core.themes', ['', ''])
     Date.now.andReturn(new Date(1994, 4, 22, 23).getTime())
     ThemeFlux.activate()
+    advanceClock(100) // wait 100ms, otherwise themes go haywire
     expect(atom.config.get('core.themes')).toEqual(['one-dark-ui', 'one-dark-syntax'])
 
     Date.now.andReturn(new Date(1994, 4, 22, 10).getTime())
     advanceClock(minutesToMilliseconds(ThemeFlux.getCheckIntervalInMinutes()))
+    advanceClock(100) // wait 100ms, otherwise themes go haywire
     expect(atom.config.get('core.themes')).toEqual(['one-light-ui', 'one-light-syntax'])
 
     Date.now.andReturn(new Date(1994, 4, 22, 18).getTime())
     advanceClock(minutesToMilliseconds(ThemeFlux.getCheckIntervalInMinutes()))
+    advanceClock(100) // wait 100ms, otherwise themes go haywire
     expect(atom.config.get('core.themes')).toEqual(['one-dark-ui', 'one-dark-syntax'])
 
     Date.now.andReturn(new Date(1994, 4, 23, 5).getTime())
     advanceClock(minutesToMilliseconds(ThemeFlux.getCheckIntervalInMinutes()))
+    advanceClock(100) // wait 100ms, otherwise themes go haywire
     expect(atom.config.get('core.themes')).toEqual(['one-dark-ui', 'one-dark-syntax'])
 
     Date.now.andReturn(new Date(1994, 4, 23, 7).getTime())
     advanceClock(minutesToMilliseconds(ThemeFlux.getCheckIntervalInMinutes()))
+    advanceClock(100) // wait 100ms, otherwise themes go haywire
     expect(atom.config.get('core.themes')).toEqual(['one-light-ui', 'one-light-syntax'])
   })
 })
